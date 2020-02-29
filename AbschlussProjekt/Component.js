36f559eff09044549e60b8c071194931e3100414
Mats Thieme <mats.thieme.mt@gmail.com>
2020-02-29 16:23:37 +0100

    .

--- AbschlussProjekt/Component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Component {
-    constructor(gameObject) {
-        this.gameObject = gameObject;
-    }
-}
-exports.Component = Component;
-//# sourceMappingURL=Component.js.map
--- AbschlussProjekt/Resources/Image.js
+++
@@ -1,7 +0,0 @@
-export class Image {
-    constructor(file) {
-        this.file = file;
-        this.image = new window.Image();
-        this.image.src = file.dataURL;
-    }
-}
