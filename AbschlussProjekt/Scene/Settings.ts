c29d1d93a2fc67e7b25147356dc4267ae3d16057
Mats Thieme <mats.thieme.mt@gmail.com>
2020-03-18 13:31:14 +0100

    moved asset directory

--- AbschlussProjekt/Scene/Settings.ts
+++ AbschlussProjekt/Scene/Settings.ts
@@ -1,6 +1,4 @@
-import { Vector2 } from './Vector2.js';
-
 export class Settings {
     public static volume: number = 1;
-    public static assetPath: string = '/Scene/Assets/';
+    public static assetPath: string = '/Assets/';
 } 
